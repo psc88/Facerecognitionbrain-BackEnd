1728951baa9a4ebef1f8cd32961dd490c6507113
Pablo Castillo <psc88sdrd@gmail.com>
2021-05-24 21:37:54 -0300

    Creacion del proyecto Back end

---
+++ server.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'pablo',
+            email: 'pablo@gmail.com',
+            password: '123456',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'flor',
+            email: 'flor@gmail.com',
+            password: '654321',
+            entries: 0,
+            joined: new Date()
+        }
+    ],
+}
+
+
+app.use(bodyParser.json());
+app.use(cors())
+
+
+app.get('/', (req, res)=> {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('success');
+        } else {
+            res.status(400).json('error logging in');
+        }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password} = req.body
+    
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.put('/image', (req, res) =>{
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+
+app.listen(3001, () => {
+    console.log('app is running on port 3000')
+})
+
+
+/*
+
+/         --> res = this is working
+/signin   --> POST = success/fail
+/register --> POST = user
+/profile/:userID --> GET =user
+/image --> PUT --> user
+
+*/
+
+//con esto podemos encriptar la contraseña---------------
+// bcrypt.hash(password, null, null, function(err, hash) {
+//     console.log(hash);
+//     });
+//--------------------------------------------------------
+
+//con esto podemos comprar el hash de el primero parmetro con el segundo
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+//     });
