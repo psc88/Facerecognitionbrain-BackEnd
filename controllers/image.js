0d98d7b1e2b5dfc9087e36e0d1ec7999a2b48bbb
Pablo Castillo <psc88sdrd@gmail.com>
2021-05-31 12:52:56 -0300

    Colocacion de clave API clarifai

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require ('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'ea059ed6721445938c7f137316717960'
+  });
+    
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) =>{
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +25,6 @@ const handleImage = (req, res, db) =>{
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
