acff8a52529fcc495fefd722a658411238e76526
Pablo Castillo <psc88sdrd@gmail.com>
2021-06-02 18:00:16 -0300

    reparar database

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : 'process.env.DATABASE_URL',
+      connectionString : process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false
-      } 
+      }
     }
   });
 
